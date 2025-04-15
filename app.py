3ba126d7d1ca2330265b91fee69a56fa730f0c8a
Felipe Ortiz <felipe.ortizh383@gmail.com>
2025-04-15 12:03:04 -0500

    Organizacion de archivos y creacion de archivo python

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+from flask import Flask, render_template
 
-print("HELLO WORLD")
+app = Flask(__name__)
+
+app.config.from_pyfile('config.py') 
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=app.config['DEBUG'])
