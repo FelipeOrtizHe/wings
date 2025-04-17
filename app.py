50059ccc9ec6776127e0a8cba699e9826d642195
Felipe Ortiz <felipe.ortizh383@gmail.com>
2025-04-16 19:46:11 -0500

    pruebas registro usuario

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from models import Usuario, Post
 from models.forms import Registro
 from werkzeug.security import generate_password_hash
 from utils.secret import SECRET_KEY
+from sqlalchemy.exc import IntegrityError 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -15,7 +16,7 @@ app.config.from_pyfile('utils/secret.py')
 db.init_app(app)
 
 
-@app.route("/")
+@app.route("/home")
 def hello_world():
     return "<p>Hello, World!</p>"
 
@@ -37,10 +38,19 @@ def registro():
         nuevo_usuario = Usuario(nombre = form.nombre.data,
                                 correo_electronico = form.correo_electronico.data,
                                 contraseña = hashed_contraseña,
-                                biografi = form.biografia.data)
+                                biografia = form.biografia.data)
         db.session.add(nuevo_usuario)
-        db.session.commit()
-        return redirect(url_for("/"))
+        try:
+            db.session.commit()
+            return redirect(url_for("hello_world"))
+        except IntegrityError as e:
+            db.session.rollback()
+            if "Duplicate entry" in str(e) and "nombre_UNIQUE" in str(e):
+                form.nombre.errors.append('Este nombre de usuario ya está en uso.')
+            elif "Duplicate entry" in str(e) and "correo_electronico_UNIQUE" in str(e):
+                form.correo_electronico.errors.append('Este correo electrónico ya está registrado.')
+            else:
+                form.errors.append('Ocurrió un error al registrar el usuario.')
     return render_template('registro.html', title= 'Registro', form = form)
 
 if __name__ == '__main__':
